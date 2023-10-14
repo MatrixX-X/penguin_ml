d57b82ba03469bd8149df6dbf475d98ea357eb4f
Abdul Mateen <abdmateenmulla@gmail.com>
2023-10-15 00:25:07 +0530

    changes

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -10,6 +10,7 @@ st.title('Penguin Classifier')
 st.write("This app uses 6 inputs to predict the species of penguin using "
  "a model built on the Palmer's Penguin's dataset. Use the form below"
  " to get started!")
+penguin_file = 0
 penguin_file = st.file_uploader('Upload your own penguin data')
 
 if penguin_file is None:
